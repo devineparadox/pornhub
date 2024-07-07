a5466945fad890aa298f2b06d7f3a96e7cd2f31d
Devine <devineparadoxz@gmail.com>
2024-07-07 14:39:17 +0530

    Update __version__.py

--- PornHub/__version__.py
+++ PornHub/__version__.py
@@ -1,3 +1,7 @@
-# PornHub/__version__.py
-__version__ = "1.0.0"
-__version_code__ = 1
+__version__ = "2.0.1b4"
+__version_code__ = (
+    run(["git", "rev-list", "--count", "HEAD"], capture_output=True)
+    .stdout.decode()
+    .strip()
+    or "0"
+)
