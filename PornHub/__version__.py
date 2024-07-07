098370ada1d5d80d87f9caf0be117e9fe0985106
Devine <devineparadoxz@gmail.com>
2024-07-07 14:48:58 +0530

    Update __version__.py

--- PornHub/__version__.py
+++ PornHub/__version__.py
@@ -1,3 +1,8 @@
+import subprocess
+from subprocess import run
+
+result = subprocess.run(["git", "rev-list", "--count", "HEAD"], capture_output=True, text=True)
+__version__ = result.stdout.strip()
 __version__ = "2.0.1b4"
 __version_code__ = (
     run(["git", "rev-list", "--count", "HEAD"], capture_output=True)
